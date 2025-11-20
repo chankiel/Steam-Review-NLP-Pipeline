0838b8839fcff7c1a8ce426718b1f9e238956f86
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2025-11-20 21:16:33 +0700

    feat: add overall repo skeleton for ml service

---
+++ src/utils/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+logger = logging.getLogger("steam-analysis")
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("[%(levelname)s] %(message)s"))
+
+logger.addHandler(handler)
