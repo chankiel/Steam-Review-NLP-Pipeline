799b7a895ac2953c882b7d86211392a4e5781134
Matthew Vladimir <117185466+NgokNgok04@users.noreply.github.com>
2025-11-26 12:22:37 +0700

    feat: summarizer driver

--- src/preprocess/group_summarizer.py
+++ src/preprocess/group_summarizer.py
@@ -12,7 +12,7 @@ def clean_text(text: str) -> str:
     return text.strip()
 
 
-def group_reviews(df, max_reviews_per_game=20):
+def group_reviews(df, cleaner=clean_text, max_reviews_per_game=5):
     df = df.copy()
     df["app_name"] = df["app_name"].fillna("Unknown")
 
@@ -25,5 +25,16 @@ def group_reviews(df, max_reviews_per_game=20):
           .reset_index()
     )
 
-    grouped["clean_text"] = grouped["review_text"].apply(clean_text)
+    grouped["clean_text"] = grouped["review_text"].apply(cleaner)
     return grouped
+
+
+def clean_text_textrank(text: str) -> str:
+    text = str(text)
+    
+    text = text.replace("|||", ". ")
+    
+    text = re.sub(r"http\S+", "", text)
+    text = text.replace("\n", " ")
+    text = re.sub(r"\s+", " ", text)
+    return text.strip()
